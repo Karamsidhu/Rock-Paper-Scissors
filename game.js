9c6c5706ba6236f63fc94399510cb9f82ea837d2
Karam.Sidhu <Karam02sidhu@gmail.com>
2024-05-05 17:27:58 -0600

    done

--- game.js
+++ game.js
@@ -1,32 +1,49 @@
 let humanScore = 0; 
 let computerScore = 0;
 
+const dom = document.querySelector(".DOMdisplay");
+const dom_text = document.querySelector(".DOMtext");
+const dom_butt = document.querySelector(".DOMbutts");
+
+let rock = document.createElement("button");
+rock.textContent = "Rock";
+
+let paper = document.createElement("button");
+paper.textContent = "Paper";
+
+let sciss = document.createElement("button");
+sciss.textContent = "Scissors";
+
+
+function playGame() {
+    dom_text.removeChild(userScore);
+    dom_text.removeChild(roboScore);
+
+    dom_butt.removeChild(rock);
+    dom_butt.removeChild(paper);
+    dom_butt.removeChild(sciss);
+
+    userScore = document.createElement("a");
+    userScore.textContent = `User Score: ${humanScore}`;
+
+    roboScore = document.createElement("a");
+    roboScore.textContent = `Robo Score: ${computerScore}`;
+
+    dom_text.appendChild(userScore);
+    dom_text.appendChild(roboScore);
+
+    dom_butt.appendChild(rock);
+    dom_butt.appendChild(paper);
+    dom_butt.appendChild(sciss);
+}
+
 
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
 
     if (rand == 1) {
-        return "rock"
+        return 1
     } else if (rand == 0) {
-        return "paper"
-    } else {
-        return "scissors"
-    }
-}
-
-
-
-function getHumanChoice() {
-    let input = prompt("1:Rock, 2:Paper, 3:Scissors\n Input a numeric value:");
-
-    return input
-}
-
-
-function helper_coding(choice) {
-    if (choice == "rock") {
-        return 1        
-    } else if (choice == "paper") {
         return 2
     } else {
         return 3
@@ -34,64 +51,64 @@ function helper_coding(choice) {
 }
 
 
-function playRound(human, computer) {
-    if (human == 1 && computer == 3) {
-        console.log("You Win! Paper beats Rock.")
-        ++humanScore;
-    } else if (human == 1 && computer == 2) {
-        console.log("You Lose! Rock beats Paper.")
-        ++computerScore;
-    } else if (human == 1 && computer == 1) {
-        console.log("Tie Game! Rock Stalemate.")
+function press(val) {
+    let robo = getComputerChoice();
+
+    switch (val) {
+        case 1:
+            if (robo == 1) {
+
+            } else if (robo == 2) {
+                ++computerScore;
+            } else {
+                ++humanScore;
+            }
+            playGame();
+            break;
+
+        case 2:
+            if (robo == 1) {
+                ++humanScore;
+            } else if (robo == 2) {
+                ++computerScore;
+            } else {
+                ++humanScore;
+            }
+            playGame();
+            break;
+
+        case 3:
+            if (robo == 1) {
+            } else if (robo == 2) {
+                ++computerScore;
+            } else {
+                ++humanScore;
+            }
+            playGame();
+            break;
     
-    } else if (human == 2 && computer == 1) {
-        console.log("You Lose! Paper beats Rock.")
-        ++computerScore;
-    }  else if (human == 2 && computer == 2) {
-        console.log("Tie Game! Paper Stalemate.")
-    } else if (human == 3 && computer == 2) {
-        console.log("You Win! Scissors beats Paper.")
-        ++humanScore;
-    
-    } else if (human == 3 && computer == 1) {
-        console.log("You Lose! Rock beats Scissors.")
-    } else if (human == 3 && computer == 2) {
-        console.log("You Win! Scissors beats Paper.")
-    } else if (human == 3 && computer == 3) {
-        console.log("Tie Game! Scissors Stalemate.")
-    } 
-}
-
-
-function playGame() {
-
-    let humanSelect = helper_coding(getHumanChoice());
-    let computerSelect = helper_coding(getComputerChoice());
-    playRound(humanSelect, computerSelect);
-
-    humanSelect = helper_coding(getHumanChoice());
-    computerSelect = helper_coding(getComputerChoice());
-    playRound(humanSelect, computerSelect);
-
-    humanSelect = helper_coding(getHumanChoice());
-    computerSelect = helper_coding(getComputerChoice());
-    playRound(humanSelect, computerSelect);
-
-    humanSelect = helper_coding(getHumanChoice());
-    computerSelect = helper_coding(getComputerChoice());
-    playRound(humanSelect, computerSelect);
-
-    humanSelect = helper_coding(getHumanChoice());
-    computerSelect = helper_coding(getComputerChoice());
-    playRound(humanSelect, computerSelect);
-
-    if (humanScore > computerScore) {
-        console.log("You Won more rounds!")
-    } else if (humanScore == computerScore) {
-        console.log("Tie game! Stalemate!")
-    } else {
-        console.log("You Lost more rounds!")
+        default:
+            break;
     }
 }
+
+sciss.addEventListener("click", function() { press(3)} );
+paper.addEventListener("click", function() { press(2)} );
+rock.addEventListener("click",  function() { press(1)} );
+
+
+let userScore = document.createElement("a");
+userScore.textContent = `User Score: ${humanScore}`;
+
+let roboScore = document.createElement("a");
+roboScore.textContent = `Robo Score: ${computerScore}`;
+
+dom_text.appendChild(userScore);
+dom_text.appendChild(roboScore);
+
+dom_butt.appendChild(rock);
+dom_butt.appendChild(paper);
+dom_butt.appendChild(sciss);
+
 
 playGame();
